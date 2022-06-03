4950385a6357763f4d56654bc3d6128047908f92
Rian Carter <R.Carter86@hotmail.com>
2022-06-03 09:18:43 -0700

    build out CRUD functionality for reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+
+  def show
+    @reivew = Review.find(params[:id])
+    @product = @review.product
+  end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review added."
+      redirect_to product_path(@review.product)
+    else
+      flash[:notice] = "Product not added."
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "Review updated."
+      redirect_to product_review_path(@product, @preview)
+    else
+      flash[:notice] = "Product not added."
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+    end
+  end
