86df5b8944588b69b8827d626675a8989fb0d95b
Rian Carter <R.Carter86@hotmail.com>
2022-06-03 15:28:25 -0700

    add test for errors if description left blank

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -8,4 +8,12 @@ describe "edit product process" do
     click_on 'Update Product'
     expect(page).to have_content 'French Fries'
   end
+
+  it "returns an error if description is left blank" do
+    product = Product.create(:name => 'Brie', :cost => '3.95', :origin => 'France')
+    visit edit_product_path(product)
+    fill_in 'Name', :with => ''
+    click_button 'Update Product'
+    expect(page).to have_content 'errors'
+  end
 end
