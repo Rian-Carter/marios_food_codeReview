738233b5ba305ceb286dd89862a419444b758eb5
Rian Carter <R.Carter86@hotmail.com>
2022-06-02 21:07:44 -0700

    add root to and resources statements

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root to: 'products#index'
+#   resources :products do
+#     resources :reviews, :except => [:index]
+#   end
+# end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'products#index'
+  resources :products do
+    resources :reviews
+  end
 end
