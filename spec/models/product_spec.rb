bcec7c9f287113b6b79c9e899cfca642e38a8e7d
Rian Carter <R.Carter86@hotmail.com>
2022-06-03 15:18:38 -0700

    realized error, fixed test to test properly

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,3 +7,10 @@ describe Product do
   
   it {should have_many(:reviews)}
 end
+
+describe Product do
+  it("titleizes the name of a product") do
+    product = Product.create({name: "brie", cost: "1.25", origin: "united states of america"})
+    expect(product.name()).to(eq("Brie"))
+  end
+end
