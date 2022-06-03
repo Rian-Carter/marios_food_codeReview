14ec3f2a5160cc46031caeaa9be9f1446be6712d
Rian Carter <R.Carter86@hotmail.com>
2022-06-02 22:19:31 -0700

    add tests for validating author, content_body, rating

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 describe Review do
+  it {should validate_presence_of :author}
+  it {should validate_presence_of :content_body}
+  it {should validate_presence_of :rating}
+
   it{should belong_to(:product)}
 end
