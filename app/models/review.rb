c7415998eab491ab29fd39de56e394a5e322557e
Rian Carter <R.Carter86@hotmail.com>
2022-06-03 15:15:55 -0700

    change code for testing purposes

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ApplicationRecord
   validates :author, :presence => true
   validates :content_body, {:presence => true, :length => { :minimum => 50, :maximum => 250 }}
   validates :rating, :presence => true
-  validates :rating, :inclusion => {:in => [1,2,3,4,5]}
+  # validates :rating, :inclusion => {:in => [1,2,3,4,5]}
+  validates :rating, :presence => true
 end
