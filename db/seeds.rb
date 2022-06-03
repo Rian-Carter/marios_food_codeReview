a6a3a6585a2f01012d4dad8fd245721934366f76
Rian Carter <R.Carter86@hotmail.com>
2022-06-03 13:36:54 -0700

    incorporate seeding with faker, not positive address.country is working correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+50.times do
+  product = Product.create!(
+    name: Faker::Food.unique.ingredient,
+    cost: Faker::Number.decimal(l_digits: 2),
+    origin: Faker::Address.country
+  )
+  5.times do
+    Review.create!(
+      author: Faker::FunnyName.name,
+      content_body: Faker::Lorem.paragraph_by_chars(number:50),
+      rating: Faker::Number.between(from: 1, to: 5),
+      product_id: product.id
+    )
+  end
+  p "Created #{Review.count} reviews"
+end
+p "Created #{Product.count} products"
