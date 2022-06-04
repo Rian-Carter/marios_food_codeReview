3f3b6db2f0c8481dbe9c76fac30df81f0c448b29
Rian Carter <R.Carter86@hotmail.com>
2022-06-03 19:31:16 -0700

    add test to verify error message works

--- spec/features/edit_review_pages_spec.rb
+++ spec/features/edit_review_pages_spec.rb
@@ -9,4 +9,13 @@ describe "edit review" do
     click_on 'Update Review'
     expect(page).to have_content 'Jackie'
   end
+
+  it "reutrns an error when author is left blank" do
+    product = Product.create(:name => 'Brie', :cost => '3.65', :origin => 'France')
+    review = Review.create(:author => 'Sally', :content_body => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', :rating => '4', :product_id => product.id)
+    visit edit_product_review_path(product, review)
+    fill_in 'Author', :with => ''
+    click_button 'Update Review'
+    expect(page).to have_content 'errors'
+  end
 end
