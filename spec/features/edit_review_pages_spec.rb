a3e335f1055d5c1e0b04b6a3f8c19990c8e1ad21
Rian Carter <R.Carter86@hotmail.com>
2022-06-03 19:29:07 -0700

    add test for editing a review

---
+++ spec/features/edit_review_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "edit review" do
+  it "edits a review" do
+    product = Product.create(:name => 'Brie', :cost => '3.65', :origin => 'France')
+    review = Review.create(:author => 'Sally', :content_body => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', :rating => '4', :product_id => product.id)
+    visit edit_product_review_path(product, review)
+    fill_in 'Author', :with => 'Jackie'
+    click_on 'Update Review'
+    expect(page).to have_content 'Jackie'
+  end
+end
