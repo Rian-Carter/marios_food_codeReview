663a8bf6af8e3d061641b7bb050853cab46e4eb4
Rian Carter <R.Carter86@hotmail.com>
2022-06-08 20:44:45 -0700

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'products#index'
+  root to: 'products#home'
   resources :products do
     resources :reviews
   end
