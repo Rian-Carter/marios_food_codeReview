54df7509363cb2d2aa3590bc3123b59074fe6fd7
Rian Carter <R.Carter86@hotmail.com>
2022-06-17 13:19:16 -0700

    incorporate before each block and steps to create admin account and login

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 describe "add a product" do
+  before(:each) do
+    visit new_product_path
+    click_link 'Sign-Up'
+    fill_in 'user_email', with: 'useremail@gmail.com'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_button 'Sign up'
+    @user = User.last
+    @user.update(admin: true)
+    visit new_product_path
+  end
+
   it "adds a new product" do
-    visit products_path
-    click_link "New Product"
     fill_in 'Name', :with => "Brie Cheese"
     fill_in 'Cost', :with => "3.95"
     select 'France', :from => 'product_origin'
@@ -11,8 +21,7 @@ describe "add a product" do
     expect(page).to have_content 'Products'
   end
 
-  it "returns an error to the user if fields left blank" do
-    visit new_product_path
+  it "returns an error to the user if fields are left blank" do
     click_on 'Create Product'
     expect(page).to have_content 'errors'
   end
