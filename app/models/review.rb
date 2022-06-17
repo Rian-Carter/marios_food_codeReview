c30727cf4c399a30d5ee29bdf9770a55091d0b2d
Rian Carter <R.Carter86@hotmail.com>
2022-06-17 13:00:05 -0700

    remove unwanted line of code

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,4 @@ class Review < ApplicationRecord
   validates :author, :presence => true
   validates :content_body, {:presence => true, :length => { :minimum => 50, :maximum => 250 }}
   validates :rating, :presence => true
-  # validates :rating, :inclusion => {:in => [1,2,3,4,5]}
 end
