9e5ea50064295f5ed5962c404f783d85d4d21de4
Rian Carter <R.Carter86@hotmail.com>
2022-06-17 14:11:08 -0700

    nothing changed, but spacing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :only => [:new, :edit, :create, :update, :destroy] do
     redirect_to new_user_session_path unless current_user && current_user.admin
   end
-  
+
   def index
       @products = Product.all
       render :index
