5e08aa7236573fd22a3e1e56d907428f11f0a6f1
Rian Carter <R.Carter86@hotmail.com>
2022-06-18 23:20:18 -0700

    add user and admin to db using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
 Review.destroy_all
+User.destroy_all
 
-50.times do
+admin= User.create!(:email => 'admin@test.com', :password => 'Testpassword123', :admin => true)
+user = User.create!(:email => 'user@test.com', :password => 'Testpassword123')
+50.times do |index|
   product = Product.create!(
     name: Faker::Food.unique.ingredient,
     cost: Faker::Number.decimal(l_digits: 2),
     origin: Faker::Address.country
   )
-  5.times do
+  5.times do |index|
     Review.create!(
       author: Faker::FunnyName.name,
       content_body: Faker::Hipster.paragraph_by_chars(characters: 250, supplemental: false),
