1544df5a893fee47af392d9c7d4507d1c86b3330
Rian Carter <R.Carter86@hotmail.com>
2022-06-26 10:36:41 -0700

    add 2x loop to ensure usa products

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,19 +11,26 @@ User.destroy_all
 
 admin= User.create!(:email => 'admin@test.com', :password => 'Testpassword123', :admin => true)
 user = User.create!(:email => 'user@test.com', :password => 'Testpassword123')
-50.times do |index|
-  product = Product.create!(
+3.times do |index|
+  Product.create!(
     name: Faker::Food.unique.ingredient,
     cost: Faker::Number.decimal(l_digits: 2),
-    origin: Faker::Address.country
+    origin: Faker::Address.country_by_code(code: 'US')
   )
-  5.times do |index|
-    Review.create!(
-      author: Faker::FunnyName.name,
-      content_body: Faker::Hipster.paragraph_by_chars(characters: 250, supplemental: false),
-      rating: Faker::Number.between(from: 1, to: 5),
-      product_id: product.id
+  50.times do |index|
+    product = Product.create!(
+      name: Faker::Food.unique.ingredient,
+      cost: Faker::Number.decimal(l_digits: 2),
+      origin: Faker::Address.country
     )
+    5.times do |index|
+      Review.create!(
+        author: Faker::FunnyName.name,
+        content_body: Faker::Hipster.paragraph_by_chars(characters: 250, supplemental: false),
+        rating: Faker::Number.between(from: 1, to: 5),
+        product_id: product.id
+      )
+    end
   end
   p "Created #{Review.count} reviews"
 end
